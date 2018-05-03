df3a9ae3794f3b49f8b26221741c80a6fc24c4bd
Helen <habadio@mit.edu>
2018-05-03 14:14:12 -0400

    a buggy change graph implementation @niki to fix

--- js/checkbox.js
+++ js/checkbox.js
@@ -1,7 +1,7 @@
 Util.events(document, {
     "DOMContentLoaded": function() {
         var tasks = document.getElementsByClassName("completed");
-        console.log(tasks)
+        // console.log(tasks)
         for (var i = 0; i < tasks.length; i++) { 
             var currTask = tasks[i];
             currTask.classList.add("hide");
@@ -16,7 +16,7 @@ Util.events(document, {
         var x = evt.clientX;
         var y = evt.clientY;
         var task = document.elementsFromPoint(x, y);
-        console.log(task)
+        // console.log(task)
         for (var i = 0; i < task.length; i++) { 
             var classNames = task[i].className.split(" ")
             if (classNames.indexOf("completed") >= 0) {
@@ -28,27 +28,32 @@ Util.events(document, {
                 break;
             }
         }
-        var taskClasses = task.className.split(" ");
-        if (taskClasses.indexOf("completed") >= 0) {
-            clickedCompleted(task);
-        }
-        else if (taskClasses.indexOf("new-task") >= 0) {
-            console.log("hre")
-            var completed = task.getElementsByClassName("completed")[0];
-            var completedClasses = completed.className.split(" ");
+        try {
+            var taskClasses = task.className.split(" ");
+            if (taskClasses.indexOf("completed") >= 0) {
+                clickedCompleted(task);
+            }
+            else if (taskClasses.indexOf("new-task") >= 0) {
+                console.log("hre")
+                var completed = task.getElementsByClassName("completed")[0];
+                var completedClasses = completed.className.split(" ");
 
-            if (completedClasses.indexOf("hide") >= 0) {
-                resetCompleted();
-                completed.classList.remove("hide");
-                completed.classList.add("show");
-            } 
+                if (completedClasses.indexOf("hide") >= 0) {
+                    resetCompleted();
+                    completed.classList.remove("hide");
+                    completed.classList.add("show");
+                } 
+                else {
+                    completed.classList.remove("show");
+                    completed.classList.add("hide");
+                }
+            }
             else {
-                completed.classList.remove("show");
-                completed.classList.add("hide");
+                resetCompleted();
             }
         }
-        else {
-            resetCompleted();
+        catch(err) {
+            console.log("Error occurred: ", err)
         }
     },
 
@@ -118,5 +123,19 @@ function resetCompleted() {
     for (var i = 0; i < openTasks.length; i++) { 
         openTasks[i].classList.add("hide");
         openTasks[i].classList.remove("show");
+    }
+}
+
+function changePicture(type) {
+    console.log("here")
+    var walkPic = document.getElementById("walk-pic")
+    if (type == "day") {
+        walkPic.src = "images/walk_day.png";
+    }
+    else if (type == "week") {
+        walkPic.src = "images/walk_week.png";
+    }
+    else if (type == "month") {
+        walkPic.src = "images/miles_week.png";
     }
 }
